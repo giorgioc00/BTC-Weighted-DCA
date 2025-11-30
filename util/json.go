88a20d3e04359015c683061b584c298bc92d25e5
Giorgio Contenti <giorgio@ofpfunding.com>
2025-11-30 22:00:08 +0300

    Chore: comments

--- util/json.go
+++ util/json.go
@@ -6,14 +6,13 @@ import (
 )
 
 // LoadJSONConfig reads a JSON file and unmarshals it into the provided target
-// The target parameter should be a pointer to the struct you want to populate
 func LoadJSONConfig(filepath string, target interface{}) error {
 	content, err := os.ReadFile(filepath)
 	if err != nil {
 		return err
 	}
 
-	err = json.Unmarshal(content, target)
+	err = json.Unmarshal(content, target) // it wants a pointer
 	if err != nil {
 		return err
 	}
