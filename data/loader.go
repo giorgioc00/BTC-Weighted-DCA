697a2fc476c0350a22070508f8d8c89ece98ad38
Giorgio Contenti <giorgio@ofpfunding.com>
2025-12-03 21:58:12 +0300

    Refactor: use util instead of duplicate the logic

--- data/loader.go
+++ data/loader.go
@@ -1,34 +1,14 @@
 package data
 
 import (
-	"bufio"
-	"encoding/csv"
-	"os"
-	"strconv"
-	"strings"
+	"backtester/util"
 )
 
 // LoadPricesFromCSV reads price data from a CSV file
 // Returns prices in chronological order (oldest first)
 func LoadPricesFromCSV(filepath string) ([]float64, error) {
-	file, err := os.Open(filepath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
 
-	// Skip BOM if present
-	br := bufio.NewReader(file)
-	r, _, err := br.ReadRune()
-	if err != nil {
-		return nil, err
-	}
-	if r != '\uFEFF' {
-		br.UnreadRune() // Not a BOM, put it back
-	}
-
-	reader := csv.NewReader(br)
-	records, err := reader.ReadAll()
+	records, err := util.LoadCSVColumn("data/btc_price.csv", "Price", true)
 	if err != nil {
 		return nil, err
 	}
@@ -37,19 +17,11 @@ func LoadPricesFromCSV(filepath string) ([]float64, error) {
 	var prices []float64
 	for i := 1; i < len(records); i++ {
 		// Price is column 1 (index 1), format: "87,342.0"
-		priceStr := records[i][1]
-		priceStr = strings.ReplaceAll(priceStr, ",", "") // Remove commas
-
-		price, err := strconv.ParseFloat(priceStr, 64)
+		price := records[i]
 		if err != nil {
 			continue // Skip invalid rows
 		}
 		prices = append(prices, price)
-	}
-
-	// Reverse to get chronological order (oldest first)
-	for i, j := 0, len(prices)-1; i < j; i, j = i+1, j-1 {
-		prices[i], prices[j] = prices[j], prices[i]
 	}
 
 	return prices, nil
