4f457af42d6169f350bae1e35a2b95cc9fc09e35
Giorgio C. <dev@giorgiocontenti.com>
2025-12-10 20:05:21 +0300

    Fix data loading and config validation

--- indicators/fearAndGreed.go
+++ indicators/fearAndGreed.go
@@ -1,6 +1,11 @@
 package indicators
 
 func FearAndGreed(prices []float64) []float64 {
+	values := make([]float64, len(prices))
 
-	return make([]float64, len(prices))
+	for i := range values {
+		values[i] = 50.0
+	}
+
+	return values
 }
